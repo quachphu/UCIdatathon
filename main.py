ae817fcf39123b035b76221e2220622396fc0f5a
Quach Thien Phu <quachphuwork@gmail.com>
2025-04-12 11:55:34 -0700

    git commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import seaborn as sns
+import numpy as np
+import altair as alt
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+
+
+df = pd.read_csv("ds_challenge_v2_1_data.csv")
+df.shape 
+
+
+
